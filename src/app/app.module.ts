d77f918ce7fcf39b9a46bd1a22080e6b74404daf
-KaiK- <enriqueoriol@gmail.com>
2018-06-05 07:37:48 +0200

    created home page and lazy module with its lazy page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+let routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'lazy',
+    loadChildren: `lazy/lazy.module#LazyModule`
+  }
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' })
---
+++ src/app/lazy/lazy.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LazyPageComponent } from './lazy-page/lazy-page.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LazyPageComponent]
+})
+export class LazyModule { }
